efd29f658b7720f1f642545d3791979bb526102b
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-06-24 17:15:20 +0200

    change model User

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User', {
-	username: String,
-	email: String,
-	password: String,
+	email: {
+		unique: true,
+		type: String,
+	},
+	account: {
+		username: {
+			required: true,
+			type: String,
+		},
+		avatar: Object,
+	},
 	newsletter: Boolean,
+	token: String,
+	hash: String,
+	salt: String,
 });
 
 module.exports = User;
